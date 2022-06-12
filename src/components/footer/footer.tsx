6c57398f14f0bf18809d2e17e500e1556577467f
Joaquin A. Moquette <joaquin@moquette.us>
2022-06-12 09:05:12 -0700

    chore: refactor

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,15 +3,9 @@ import './styles.scss'
 
 import app from '../../../package.json'
 
-interface FooterProps {
-  className?: string
-}
-
-export const Footer: React.FunctionComponent<FooterProps> = ({
-  className = 'app-footer',
-}) => {
+export const Footer = () => {
   return (
-    <footer className={`${className}`}>
+    <footer className="app-footer">
       <p>
         &copy;{new Date().getFullYear()} {app.name} v{app.version}
       </p>
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,16 +2,10 @@ import './styles.scss'
 
 import app from '../../../package.json'
 
-interface HeaderProps {
-  className?: string
-}
-
-export const Header: React.FunctionComponent<HeaderProps> = ({
-  className = 'app-header',
-}) => {
+export const Header = () => {
   return (
-    <header className={`${className}`}>
-      <h1 className=" pb-5 font-bold uppercase">{app.name}</h1>
+    <header className="app-header">
+      <h2 className="app-header__title">{app.name}</h2>
     </header>
   )
 }
