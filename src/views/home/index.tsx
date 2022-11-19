cd82ec1ce51a321d0d9ae58ed60b865ecf0a77e4
Joaquin A. Moquette <joaquin@moquette.us>
2022-11-19 15:20:46 -0800

    chore: removed console log comment

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -3,7 +3,6 @@ import './styles.scss'
 import Layout from '../../layout'
 
 const Home = () => {
-  console.log('done')
   return (
     <Layout documentTitle="Welcome" className="home">
       &lt; it works /&gt;
